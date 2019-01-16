fc11c40853c80b6f03f8d7753be617841092f5cd
Raju-mac <rajushetty.b@gmal.com>
2019-01-16 22:19:05 +0530

    ads updated

--- js/admob.js
+++ js/admob.js
@@ -4,8 +4,8 @@ var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) {
   admobid = { // for Android
     banner: '',
-    interstitial: 'ca-app-pub-7048516006218968/2944594550',
-    rewardvideo: 'ca-app-pub-7048516006218968/4994498237',
+    interstitial: 'ca-app-pub-7048516006218968/4105753438',
+    rewardvideo: 'ca-app-pub-7048516006218968/2641338968',
   };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
